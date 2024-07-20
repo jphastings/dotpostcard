5a89cbd5a90fee9a9e626a501cc7ad0674bbf537
JP Hastings-Spital <jphastings@gmail.com>
2024-07-20 08:28:43 +0100

    Nest CSS

--- docs/formats/css.md
+++ docs/formats/css.md
@@ -1,3 +1,5 @@
 # CSS
 
 This output format produces the same `postcards.css` file every time, which can be used in conjunction with the [html](./html.md) format to display [./web.md] format postcards beautifully on the web.
+
+The CSS is modern, and makes use of [nesting](https://caniuse.com/css-nesting), so you may need to do some extra work if your target audience uses older browsers.
--- internal/formats/html/html.go
+++ internal/formats/html/html.go
@@ -1,6 +1,10 @@
 package html
 
-var htmlTmpl = `<div class="postcard {{ .Flip }} {{ if gt .FrontDimensions.PxHeight .FrontDimensions.PxWidth }}portrait{{ else }}landscape{{ end }}" style="--postcard: url('{{ .Filename }}'); --aspect-ratio: {{ .FrontDimensions.PxWidth }} / {{ .FrontDimensions.PxHeight }}">
-  <img src="{{ .Filename }}" loading="lazy" alt="{{ .Front.Description }}" width="500px">
-  <div class="shadow"></div>
-</div>`
+// TODO: Can this be simpler for non-flipping postcards?
+var htmlTmpl = `<input type="checkbox" id="postcard-{{.Name}}">
+<label for="postcard-{{.Name}}">
+	<div class="postcard {{ .Flip }} {{ if gt .FrontDimensions.PxHeight .FrontDimensions.PxWidth }}portrait{{ else }}landscape{{ end }}" style="--postcard: url('{{ .Name }}.webp'); --aspect-ratio: {{ .FrontDimensions.PxWidth }} / {{ .FrontDimensions.PxHeight }}">
+		<img src="{{ .Name }}.webp" loading="lazy" alt="{{ .Front.Description }}" width="500px">
+		<div class="shadow"></div>
+	</div>
+</label>`
